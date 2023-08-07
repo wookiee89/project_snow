f8fe2c63f75b09f33bd041e9d79b1dd44fc3f924
wookiee89 <jay.r.jain@gmail.com>
2023-08-06 21:03:05 -0400

    Setup database models

--- project_snow/database/models.py
+++ project_snow/database/models.py
@@ -1,42 +1,43 @@
 import uuid
 
-from sqlalchemy import Column, ForeignKey, Table, orm
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Date, orm, func
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+BaseModel = declarative_base()
 
 
-class Base(orm.DeclarativeBase):
-    """Base database model."""
+class Base(BaseModel):
+    __abstract__ = True  # This ensures that Base class doesn't map to any table
 
-    pk: orm.Mapped[uuid.UUID] = orm.mapped_column(
-        primary_key=True,
-        default=uuid.uuid4,
-    )
-
-
-potion_ingredient_association = Table(
-    "potion_ingredient",
-    Base.metadata,
-    Column("potion_id", UUID(as_uuid=True), ForeignKey("potion.pk")),
-    Column("ingredient_id", UUID(as_uuid=True), ForeignKey("ingredient.pk")),
-)
+    pk: orm.Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    created_at: orm.Mapped[DateTime] = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at: orm.Mapped[DateTime] = Column(DateTime(timezone=True), onupdate=func.now())
 
 
-class Ingredient(Base):
-    """Ingredient database model."""
+class Patient(Base):
+    __tablename__ = "patients"
 
-    __tablename__ = "ingredient"
+    first_name: orm.Mapped[str] = Column(String, nullable=False)
+    last_name: orm.Mapped[str] = Column(String, nullable=False)
+    phone_number: orm.Mapped[str] = Column(String, nullable=False)
 
-    name: orm.Mapped[str]
+    medications: orm.Mapped[relationship] = relationship("PatientMedication", back_populates="patient")
 
 
-class Potion(Base):
-    """Potion database model."""
+class PatientMedication(Base):
+    __tablename__ = "patient_medications"
 
-    __tablename__ = "potion"
+    patient_id: orm.Mapped[UUID] = Column(UUID(as_uuid=True), ForeignKey("patients.pk"), nullable=False)
+    drug_code: orm.Mapped[int] = Column(Integer, nullable=False)
+    drug_name: orm.Mapped[str] = Column(String, nullable=False)
+    quantity: orm.Mapped[int] = Column(Integer, nullable=False)
+    refills: orm.Mapped[int] = Column(Integer, nullable=True)
+    dosage: orm.Mapped[int] = Column(Integer, nullable=False)
+    start_date: orm.Mapped[Date] = Column(Date, nullable=False)
+    start_datetime: orm.Mapped[DateTime] = Column(DateTime, nullable=False)
+    end_date: orm.Mapped[Date] = Column(Date, nullable=False)
+    end_datetime: orm.Mapped[DateTime] = Column(DateTime, nullable=False)
 
-    name: orm.Mapped[str]
-    ingredients: orm.Mapped[list["Ingredient"]] = orm.relationship(
-        secondary=potion_ingredient_association,
-        backref="potions",
-        lazy="selectin",
-    )
+    patient: orm.Mapped[relationship] = relationship("Patient", back_populates="medications")
